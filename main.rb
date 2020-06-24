cc497929b4a059045c5ee0177fa83ef82e077f59
Asep Agus <a2h2hemingway@gmail.com>
2020-06-24 12:46:31 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,29 @@
+
+day = 7; 
+hari_ini ='';
+
+
+if (day === 1)
+  hari_ini = "`Senin`";
+	puts hari_ini;
+elsif (day === 2)
+	hari_ini = "`Selasa`";
+	puts hari_ini;
+elsif (day === 3)
+	hari_ini = "`Rabu`";
+	puts hari_ini;
+elsif (day === 4)
+	hari_ini = "`Kamis`";
+	puts hari_ini;
+elsif (day === 5)
+	hari_ini = "`Jum'at`";
+	puts hari_ini;
+elsif (day === 6)
+	hari_ini = "`Sabtu`";
+	puts hari_ini;
+elsif (day === 7)
+	hari_ini = "`Minggu`";
+	puts hari_ini;
+else
+	puts hari_ini = "`Hari tidak ditemukan`";
+end
